2c43f7dd0a0a1489118fa688d7fe79cd014445ff
TryFailTryAgain <115508207+TryFailTryAgain@users.noreply.github.com>
2023-09-05 23:29:21 -0400

    Adds event file gathering

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ for (const folder of commandFolders) {
 	}
 } // End of command file loading
 
+/* Events loading */
+// Gets all the event files in the events directory
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+/* End Events Loading */
 
 
 // Actions/events/commands setup to be recognized by the bot
