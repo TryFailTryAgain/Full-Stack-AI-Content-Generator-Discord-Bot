1e25e21998d3eb1d71d5c24945375839d897460c
TryFailTryAgain <115508207+TryFailTryAgain@users.noreply.github.com>
2023-09-05 23:53:20 -0400

    Updates to add new SDXL engines

--- AIMemeGenerator.py
+++ AIMemeGenerator.py
@@ -272,8 +272,8 @@ def initialize_api_clients(apiKeys, image_platform):
         stability_api = client.StabilityInference(
             key=apiKeys.stability_key, # API Key reference.
             verbose=True, # Print debug messages.
-            engine="stable-diffusion-xl-1024-v0-9", # Set the engine to use for generation.
-            # Available engines: stable-diffusion-xl-1024-v0-9 stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
+            engine="stable-diffusion-xl-1024-v1-0", # Set the engine to use for generation.
+            # Available engines: stable-diffusion-xl-1024-v1-0 stable-diffusion-xl-1024-v0-9 stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
             # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-diffusion-xl-beta-v2-2-2 stable-inpainting-v1-0 stable-inpainting-512-v2-0
         )
     else:
