6bf5ce99c58c77f8d05e019217ab1c8f64528048
TryFailTryAgain <115508207+TryFailTryAgain@users.noreply.github.com>
2026-02-09 16:33:55 -0500

    removes import from global scope

--- functions/image_providers/OpenAI.js
+++ functions/image_providers/OpenAI.js
@@ -1,5 +1,6 @@
 const sharp = require('sharp');
 const OpenAI = require('openai');
+const { checkThenSave_ReturnSendImage } = require('../helperFunctions.js');
 
 const openaiImage = new OpenAI({ apiKey: process.env.API_KEY_OPENAI_IMAGE });
 openaiImage.baseURL = process.env.ADVCONF_OPENAI_IMAGE_BASE_URL;
@@ -23,7 +24,7 @@ async function generateImageViaDallE3({ userInput, trueDimensions, numberOfImage
             quality: "standard", // "standard" or "hd" TODO: Add this to command options
             style: "natural", // "natural" or "vivid  TODO: Add this to command options
             response_format: "b64_json",
-            user: toString(userID),
+            user: String(userID),
         });
         // Process and save the generated image
         const saveBuffer = await sharp((Buffer.from(response.data[0].b64_json, 'base64')))[process.env.ADVCONF_SAVE_IMAGES_AS]({ quality: parseInt(process.env.ADVCONF_JPEG_QUALITY) }).toBuffer();
